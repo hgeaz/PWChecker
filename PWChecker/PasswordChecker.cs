67a88e2ddbef63717731ac4c72adec2b9a3eb7ad
hgeaz <haydngeaslin@ascendcollegeprep.com>
2026-02-19 20:03:49 -0700

    Fixed things

--- PWChecker/PasswordChecker.cs
+++ PWChecker/PasswordChecker.cs
@@ -1,17 +1,27 @@
 ﻿namespace PwChecker;
 
+/// <summary>
+/// This password checker takes in a user input and determines if the typed password is a strong password. A string password has at least one
+/// special character, one or more letters, at least 1 number, and much be between 12 and 18 characters long!
+/// </summary>
 public class PasswordChecker
 {
-
     private readonly String _password;
-    
     private readonly String _specialCharacters = "!@#$%^&*()";
     
+    /// <summary>
+    /// Creates Password.
+    /// </summary>
+    /// <param name="password"></param>
     public PasswordChecker(String password)
     {
         this._password = password;
     }
 
+    /// <summary>
+    /// Returns a String explaining your if your password is too short, too long, needs a number, or needs a number.
+    /// </summary>
+    /// <returns></returns>
     public String GetStrength()
     {
         if (_password.Length < 12)
