820d9834b99683a53a03b338d8a3de03f2f3ab2e
hgeaz <haydngeaslin@ascendcollegeprep.com>
2026-02-19 19:48:32 -0700

    Made simple password checker and tester files

--- PWChecker/PasswordChecker.cs
+++ PWChecker/PasswordChecker.cs
@@ -2,4 +2,75 @@
 
 public class PasswordChecker
 {
+
+    private readonly String _password;
+    
+    private readonly String _specialCharacters = "!@#$%^&*()";
+    
+    public PasswordChecker(String password)
+    {
+        this._password = password;
+    }
+
+    public String GetStrength()
+    {
+        if (_password.Length < 12)
+        {
+            
+            return "Password is too short.";
+            
+        }
+        if (_password.Length > 18)
+        {
+            
+            return "Password is too long.";
+            
+        }
+
+        Boolean checker = false;
+        foreach (Char value in _password)
+        {
+
+            if (_specialCharacters.Contains(value))
+            {
+                
+                checker = true;
+                
+            }
+
+        }
+
+        if (!checker)
+        {
+
+            return "Password Needs at least one special character - !@#$%^&*()";
+
+        }
+        
+        checker = false;
+        foreach (Char value in _password)
+        {
+
+            if (double.TryParse(value.ToString(), out double _))
+            {
+                
+                checker = true;
+                
+            }
+
+        }
+
+        if (!checker)
+        {
+
+            return "Password needs at least one number!";
+
+        }
+
+        return "Great password!";
+
+
+    }
+
+
 }
--- PwCheckerTests/Tester.cs
+++ PwCheckerTests/Tester.cs
@@ -1,10 +1,57 @@
-﻿namespace PwCheckerTests;
+﻿using PwChecker;
+
+namespace PwCheckerTests;
 
 [TestClass]
 public sealed class Tester
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Password_TooShort_ReturnsError()
     {
+        var checker = new PasswordChecker("Abc!123");
+            
+        var result = checker.GetStrength();
+            
+        Assert.AreEqual("Password is too short.", result);
+    }
+
+    [TestMethod]
+    public void Password_TooLong_ReturnsError()
+    {
+        var checker = new PasswordChecker("Abcdefghijklmnop!123");
+            
+        var result = checker.GetStrength();
+            
+        Assert.AreEqual("Password is too long.", result);
+    }
+
+    [TestMethod]
+    public void Password_MissingSpecialCharacter_ReturnsError()
+    {
+        var checker = new PasswordChecker("Abcdefgh1234");
+            
+        var result = checker.GetStrength();
+            
+        Assert.AreEqual("Password Needs at least one special character - !@#$%^&*()", result);
+    }
+
+    [TestMethod]
+    public void Password_MissingNumber_ReturnsError()
+    {
+        var checker = new PasswordChecker("Abcdefghijkl!");
+            
+        var result = checker.GetStrength();
+            
+        Assert.AreEqual("Password needs at least one number!", result);
+    }
+
+    [TestMethod]
+    public void Password_Valid_ReturnsSuccess()
+    {
+        var checker = new PasswordChecker("Abcdefgh12!2");
+            
+        var result = checker.GetStrength();
+            
+        Assert.AreEqual("Great password!", result);
     }
 }
